acc0bf49f87cee8fba63a4fa8f79b0131f3559a4
Summer Kim <summertykim@gmail.com>
2022-11-22 17:48:34 +0000

    Completed prep-javasrcript-arrays update.

--- prep-javascript-arrays/main.js
+++ prep-javascript-arrays/main.js
@@ -6,5 +6,5 @@ var thirdElement = languages[2];
 console.log('the 3rd element of the array is', thirdElement);
 var length = languages.length;
 console.log('The length of the array is', length);
-var lastElement = languages[5];
+var lastElement = languages[length - 1];
 console.log('The last element of the array is', lastElement);
